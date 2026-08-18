88b51baf2fb274f7032e62a89742fa9d369aa5ae
Priscila <Martinez.priscila@gmail.com>
2026-08-17 17:04:21 -0700

    jukebox

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,22 @@
--- TODO
+DROP TABLE IF EXISTS playlists_tracks;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS tracks;
+
+CREATE TABLE playlists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE tracks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  duration_ms INTEGER NOT NULL
+);
+
+CREATE TABLE playlists_tracks (
+  id SERIAL PRIMARY KEY,
+  playlist_id INTEGER NOT NULL REFERENCES playlists(id),
+  track_id INTEGER NOT NULL REFERENCES tracks(id),
+  UNIQUE (playlist_id, track_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -8,3 +8,39 @@ console.log("🌱 Database seeded.");
 async function seed() {
   // TODO
 }
+import { faker } from "@faker-js/faker";
+import client from "./client.js";
+import { insertplaylists} from "./queries/playlists.js";
+import { inserttracks} from "./queries/tracks.js";
+import { insertplaylist_tracks } from "./queries/playlist_tracks.js";
+
+const seedReservations = async()=>{
+    const playlistIds = []
+    const trackIds = []
+    for(let i = 0; i<30;i++){
+        const playlistname = await insertPlaylist (
+           faker.music.genre () + "Mix"
+           faker.lorem.sentence()
+        )
+          playlistIds.push(playlist.id);
+      } 
+
+       for (let i = 0; i < 30; i++) {
+        const track = await insertTrack(
+            faker.music.songName(),
+             faker.number.int({ min: 120000, max: 300000 }) // duration_ms
+    );
+    trackIds.push(track.id);
+  }
+  for (let i = 0; i < 50; i++) {
+    const playlistTrack = {
+      playlist_id: playlistIds[Math.floor(Math.random() * playlistIds.length)],
+      track_id: trackIds[Math.floor(Math.random() * trackIds.length)],
+    };
+    await insertPlaylistTrack(playlistTrack.playlist_id, playlistTrack.track_id);
+  }
+};
+
+client.connect();
+await seedJukebox();
+client.end();
